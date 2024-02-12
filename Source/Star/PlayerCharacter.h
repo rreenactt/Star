ca413c84644859aafaef529be9c8f3735694bf5e
rreenactt <reenactPi@gmail.com>
2024-02-12 16:59:45 +0900

    st making Jump

--- Source/Star/PlayerCharacter.h
+++ Source/Star/PlayerCharacter.h
@@ -29,8 +29,12 @@ public:
 	/*UPROPERTY(Replicated)
 	float RunSpeed;*/
 
+	// 서버에서 달리면 PlayerSpeedUpdateCall 함수가 호출하는 변수
 	UPROPERTY(ReplicatedUsing = PlayerSpeedUpdateCall)
 	bool isRun;
+
+	UPROPERTY(ReplicatedUsing = PlayerJumpUpdateCall)
+	bool isJump;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -50,28 +54,54 @@ public:
 
 	UFUNCTION()
 	void PlayerSpeedUpdateCall();
+	UFUNCTION()
+	void PlayerJumpUpdateCall();
 
 	UFUNCTION()
 	void PlayerSpeedUpdate();
+	void PalyerJumpUpdate();
+
+	UFUNCTION()
+	virtual void Landed(const FHitResult& Hit);
 protected:
 	UFUNCTION()
 	void RunStart();
 	UFUNCTION()
 	void RunStop();
 
-protected:
+	UFUNCTION()
+	void JumpStart();
+	void JumpEnd();
 
+// 달리기 Server, multi
+protected:
 	UFUNCTION(Reliable, Server = "ServerPlayerRunStart_I", WithValidation = "ServerPlayerRunStart_V")
 	virtual void ServerPlayerRunStart(bool run);
 	void ServerPlayerRunStart_I(bool run);
 	bool ServerPlayerRunStart_V(bool re);
+
 	UFUNCTION(Reliable, Server = "ServerPlayerRunStop_I", WithValidation = "ServerPlayerRunStop_V")
 	virtual void ServerPlayerRunStop(bool run);
 	void ServerPlayerRunStop_I(bool run);
 	bool ServerPlayerRunStop_V(bool re);
 
-
 	UFUNCTION(Reliable, NetMulticast)
 	void MultiPlayerSpeedUpdate(bool run);
 	void MultiPlayerSpeedUpdate_Implementation(bool run);
+
+// 점프 Server, multi
+protected:
+	UFUNCTION(Reliable, Server = "ServerPlayerJumpStart_I", WithValidation = "ServerPlayerJumpStart_V")
+	virtual void ServerPlayerJumpStart(bool jump);
+	void ServerPlayerJumpStart_I(bool jump);
+	bool ServerPlayerJumpStart_V(bool jum);
+
+	UFUNCTION(Reliable, Server = "ServerPlayerJumpEnd_I", WithValidation = "ServerPlayerJumpEnd_V")
+	virtual void ServerPlayerJumpEnd(bool jump);
+	void ServerPlayerJumpEnd_I(bool jump);
+	bool ServerPlayerJumpEnd_V(bool jum);
+
+	UFUNCTION(Reliable, NetMulticast)
+	virtual void MultiPlayerJumpUpdate(bool jump);
+	void MultiPlayerJumpUpdate_Implementation(bool jump);
 };
