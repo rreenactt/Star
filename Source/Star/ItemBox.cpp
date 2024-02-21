a0e630ed3597ddc9b6af58c16048ca02251fdd65
dianinba2 <dianinba2@gmail.com>
2024-02-21 18:26:04 +0900

    ItemBox
    ItemBox와 Bat(무기)가 충돌 시 로그 출력 기능 구현

---
+++ Source/Star/ItemBox.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemBox.h"
+
+// Sets default values
+AItemBox::AItemBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = false;
+
+    Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+    Box = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BOX"));
+
+    RootComponent = Trigger;
+    Box->SetupAttachment(RootComponent);
+
+    Trigger->SetBoxExtent(FVector(40.0f, 42.0f, 30.0f));
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_BOX(TEXT("/Game/InfinityBladeWeapons/Weapons/Blunt/Blunt_BoneShard/StaticMesh/SM_Blunt_BoneShardMace.SM_Blunt_BoneShardMace"));
+    if (SM_BOX.Succeeded())
+    {
+        Box->SetStaticMesh(SM_BOX.Object);
+    }
+    Box->SetRelativeLocation(FVector(0.0f, -3.5f, -30.0f));
+
+    Trigger->SetCollisionProfileName(TEXT("ItemBox"));
+    Box->SetCollisionProfileName(TEXT("NoCollision"));
+}
+
+// Called when the game starts or when spawned
+void AItemBox::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+}
+
+// Called every frame
+void AItemBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AItemBox::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+    Trigger->OnComponentBeginOverlap.AddDynamic(this, &AItemBox::OnCharacterOverlap);
+}
+
+void AItemBox::OnCharacterOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Touch"));
+}
---
+++ Source/Star/ItemBox.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBox.generated.h"
+
+UCLASS()
+class STAR_API AItemBox : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void PostInitializeComponents() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = Box)
+	UBoxComponent* Trigger;
+
+	UPROPERTY(VisibleAnywhere, Category = Box)
+	UStaticMeshComponent* Box;
+
+private:
+	UFUNCTION()
+	void OnCharacterOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+};
