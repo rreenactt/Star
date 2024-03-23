54c3d99abec6e4febf6756b226aa5ebe5731cb17
rreenactt <reenactPi@gmail.com>
2024-03-23 19:15:29 +0900

    asdf

---
+++ Source/Star/MyChatWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyChatWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STAR_API UMyChatWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
